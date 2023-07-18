34cbd23b6bcee788a786c816c6fd1afa24c7badc
Gabriel Lima <106336816+jgabriellp@users.noreply.github.com>
2023-07-18 17:07:10 -0300

    Update urls.py

--- ppw/ppw/urls.py
+++ ppw/ppw/urls.py
@@ -16,13 +16,24 @@ Including another URLconf
 """
 from django.urls import path, include
 from app_login import views
+from app_login.views import consulta_ONE, consulta_TWO, consulta_TREE, consulta_FOUR, consulta_FIVE, consulta_SIX, consulta_SEVEN, consulta_EIGTH, consulta_NINE, consulta_TEN
 
 urlpatterns = [
     # rota, view responsavel, nome
 
     # path('admin/', admin.site.urls),
-    # path('', views.listagem, name='listagem'),
+    # path('', views.home, name='home'),
     # path('login', views.login, name='login'),
     # path('cadastro', views.cadastro, name='cadastro'),
-    path('listagem', views.listagem, name='listagem'),
+    path('', views.listagem, name='listagem'),
+    path('consulta-1', consulta_ONE, name='consulta-1'),
+    path('consulta-2', consulta_TWO, name='consulta-2'),
+    path('consulta-3', consulta_TREE, name='consulta-3'),
+    path('consulta-4', consulta_FOUR, name='consulta-4'),
+    path('consulta-5', consulta_FIVE, name='consulta-5'),
+    path('consulta-6', consulta_SIX, name='consulta-6'),
+    path('consulta-7', consulta_SEVEN, name='consulta-7'),
+    path('consulta-8', consulta_EIGTH, name='consulta-8'),
+    path('consulta-9', consulta_NINE, name='consulta-9'),
+    path('consulta-10', consulta_TEN, name='consulta-10'),
 ]
